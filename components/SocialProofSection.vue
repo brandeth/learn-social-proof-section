e20aa507bee345ab2351749f4a14d9e680557f6b
Rey Garcia <jreygarcia24@gmail.com>
2023-04-27 23:30:43 +0800

    WIP - Desktop UI

--- components/SocialProofSection.vue
+++ components/SocialProofSection.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+useHead({
+  title: "Social Proof Section - Frontend Mentor | Rey Garcia",
+  meta: [
+    {
+      name: "description",
+      content: "Social Proof Section",
+    },
+  ],
+});
+</script>
+
 <template>
   <div class="SocialProofSection">
     <div class="SocialProofSection-header">
