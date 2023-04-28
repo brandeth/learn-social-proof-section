ac8c17d1f8d51dc9d55447682b78db5191d3ebac
Rey Garcia <jreygarcia24@gmail.com>
2023-04-29 00:33:26 +0800

    added attribution

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="Main">
     <SocialProofSection />
+    <footer>
+      <div class="attribution">
+        <div>
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+            >Frontend Mentor</a
+          >
+        </div>
+        <div>
+          Coded by
+          <a href="https://github.com/brandeth" target="_blank">Rey Garcia</a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
